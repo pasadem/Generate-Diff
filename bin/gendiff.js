c37e6eae74fcec47b33acecfc642ac80be07e4b9
Pavel Demianski <pasadem@ukr.net>
2021-08-25 22:48:25 +0300

    first attempt

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,36 @@
 #!/usr/bin/env node
 
+import _ from 'lodash';
+import fs from 'fs';
+import  path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { Command } from 'commander/esm.mjs';
-// import gendiff from '../index.js';
 
 const program = new Command();
-program
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference')
-  .option('-h, --help', 'output usage information')
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const getFileData = (filepath) => {
+    return fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8');
+};
+const data1 = (file) => readFile(file);
+const data2 = (file) => readFile(file);
+
+const gendiff = (data1, data2) => {
     
+    console.log(data1, data2);
+}
+
+program
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format', '[type]  output format')
+  .action((filepath1, filepath2) => {
+      gendiff(filepath1, filepath2);
+  }); 
+  
 program.parse();
-
-
